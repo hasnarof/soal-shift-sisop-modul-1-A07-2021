ea5444150b7698b0c79a3d422c5481e23b790e94
hasnarof <hasnarof@gmail.com>
2021-03-27 17:38:26 +0700

    soal1 link pindah ke readme

--- soal1/soal1.sh
+++ soal1/soal1.sh
@@ -1,20 +1,14 @@
 #!/bin/bash
 
 # 1a
-# https://regexone.com/
 grep -o -E '(ERROR|INFO).+' syslog.log
 
 
 # 1b
-# https://stackoverflow.com/questions/13242469/how-to-use-sed-grep-to-extract-text-between-two-words
 grep -o -E '(ERROR).+' syslog.log | grep -o -P '(?<=ERROR ).*(?= \()' | sort | uniq -c
-# grep -o -E '(ERROR).+' syslog.log | grep -o -P '(?<=ERROR ).*(?= \()' | wc -l
 
 
 # 1c
-# https://stackoverflow.com/questions/16317961/how-to-process-each-output-line-in-a-loop
-# https://linuxize.com/post/how-to-compare-strings-in-bash/
-# https://stackoverflow.com/questions/1898553/return-a-regex-match-in-a-bash-script-instead-of-replacing-it
 declare -A errorCount
 declare -A infoCount
 declare -A userArray
@@ -46,14 +40,11 @@ do
 done
 
 # 1d
-# https://stackoverflow.com/questions/8948975/how-do-you-do-custom-formatting-with-the-uniq-c-option
-# https://unix.stackexchange.com/questions/170043/sort-and-count-number-of-occurrence-of-lines
 (printf "Error,Count\n"
 grep -o -E '(ERROR).+' syslog.log | grep -o -P '(?<=ERROR ).*(?= \()' | sort | uniq -c | sort -nr | sed -e 's/^ *\([0-9]\+\) \(.\+\)/\2,\1/'
 ) > error_message.csv
 
 # 1e
-# https://stackoverflow.com/questions/8217049/bash-associative-array-sorting-by-value
 declare -A errorCount
 declare -A infoCount
 declare -A userArray
