af20acf6d4fed703501a31e9444b8c2800a00a2b
hasnarof <hasnarof@gmail.com>
2021-03-27 21:52:21 +0700

    test

--- soal3/soal3c.sh
+++ soal3/soal3c.sh
@@ -1,94 +1,41 @@
 #!/bin/bash
 hari=$(date +"%d-%m-%Y")
-kucing=$(ls | grep -e"Kucing.*" | wc -l)
-kelinci=$(ls | grep -e"Kucing.*" | wc -l)
-if [[ kucing -eq kelinci ]]
-then
 mkdir "Kelinci_$hari"
-for ((q=1; q<=23; q++))
-do
-        for ((b; b<q; b=b+1))
-        do
-                if ((q<10))
-                then
-                wget -O "Koleksi_0$q.jpg" https://loremflickr.com/320/240/bunny -a "Foto.log"
-                sama=$(cmp "Koleksi_0$q.jpg" "Koleksi_0$b.jpg")
-                akun=$?
-                #remove jika sama
-                if [ $akun == 0 ]
-                then
-                        rm "Koleksi_0$q.jpg"
-                        let q=$q-1
-                        
-                fi
-                else
-                wget -O "Koleksi_$q.jpg" https://loremflickr.com/320/240/bunny -a "Foto.log"
-                        if ((b<10))
-                        then
-                        sama=$(cmp "Koleksi_$q.jpg" "Koleksi_0$b.jpg"  )
-                        akun=$?
-                        #remove jika sama
-                        if [ $akun == 0 ]
-                        then
-                        rm "Koleksi_$q.jpg"
-                        let q=$q-1 
-                        fi
-                        else 
-                        sama=$(cmp "Koleksi_$q.jpg" "Koleksi_$b.jpg"  )
-                        akun=$?
-                        #remove jika sama
-                        if [ $akun == 0 ]
-                        then
-                        rm "Koleksi_$q.jpg" 
-                        let q=$q-1
-                        fi
-                fi
-                fi
-        done
-done
-mv ./Koleksi_* ./Foto.log "./Kelinci_$hari"
-else [[ kucing -ne kelinci ]]
 mkdir "Kucing_$hari"
+
+
 for ((q=1; q<=23; q++))
 do
-        for ((b; b<q; b=b+1))
-        do
-                if ((q<10))
-                then
-                wget -O "Koleksi_0$q.jpg" https://loremflickr.com/320/240/kitten -a "Foto.log"
-                sama=$(cmp "Koleksi_0$q.jpg" "Koleksi_0$b.jpg")
-                akun=$?
-                #remove jika sama
-                if [ $akun == 0 ]
-                then
-                        rm "Koleksi_0$q.jpg"
-                        let q=$q-1
-                        
-                fi
-                else
-                wget -O "Koleksi_$q.jpg" https://loremflickr.com/320/240/kitten -a "Foto.log"
-                        if ((b<10))
-                        then
-                        sama=$(cmp "Koleksi_$q.jpg" "Koleksi_0$b.jpg"  )
-                        akun=$?
-                        #remove jika sama
-                        if [ $akun == 0 ]
-                        then
-                        rm "Koleksi_$q.jpg"
-                        let q=$q-1 
-                        fi
-                        else 
-                        sama=$(cmp "Koleksi_$q.jpg" "Koleksi_$b.jpg"  )
-                        akun=$?
-                        #remove jika sama
-                        if [ $akun == 0 ]
-                        then
-                        rm "Koleksi_$q.jpg" 
-                        let q=$q-1
-                        fi
-                fi
-                fi
-        done
+    if((q % 2 == 0))
+    then
+    for((b=0; b<q; b=b+2))
+    do
+        wget -O "Koleksi_$q.jpg" https://loremflickr.com/320/240/kitten -a "Foto.log"
+        mv ./Koleksi_* "./Kucing_$hari"
+        sama=$(cmp "Koleksi_$q.jpg" "Koleksi_$b.jpg")
+        akun=$?
+        #remove jika sama
+        if [ $akun == 0 ]
+        then
+                rm "Koleksi_$q.jpg"
+                let q=$q-1
+        fi
+    done
+    
+    else
+    for((b=1; b<q; b=b+2))
+    do
+        wget -O "Koleksi_$q.jpg" https://loremflickr.com/320/240/bunny -a "Foto.log"
+        mv ./Koleksi_* "./Kelinci_$hari"
+        sama=$(cmp "Koleksi_$q.jpg" "Koleksi_$b.jpg")
+        akun=$?
+        #remove jika sama
+        if [ $akun == 0 ]
+        then
+                rm "Koleksi_$q.jpg"
+                let q=$q-1
+        fi
+    done
+    
+    fi
 done
-mv ./Koleksi_* ./Foto.log "./Kucing_$hari"
-fi
